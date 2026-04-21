01010b7cc8f4db3bb80a110795d25ad7ce0d82d0
duckgo <pixelzone@tutamail.com>
2026-04-21 11:46:38 -0300

    0.60.3

--- fg_bar.lua
+++ fg_bar.lua
@@ -1,4 +1,4 @@
 
 boss_s_hudbar.add_boss("forgotten_monsters:sking", "Skull King",1000,"boss_bar",1)
-boss_s_hudbar.add_boss("forgotten_monsters:golem", "Dark Golem",1000,"boss_bar_blue",0.8)
-boss_s_hudbar.add_boss("forgotten_monsters:meselord", "Mese Lord",1000,"boss_bar_yellow",0.45)
+boss_s_hudbar.add_boss("forgotten_monsters:golem", "Dark Golem",800,"boss_bar_blue",0.8)
+boss_s_hudbar.add_boss("forgotten_monsters:meselord", "Mese Lord",450,"boss_bar_yellow",0.45)
